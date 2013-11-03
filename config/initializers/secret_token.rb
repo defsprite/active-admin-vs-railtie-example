dc0a37ae0b39e6223f93f74e3d905bf6ba22af49
Christoph Sassenberg <christoph.sassenberg@googlemail.com>
2013-11-03 23:53:48 +0000

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ActionpackPageCachingExample::Application.config.secret_key_base = 'edbefec221341b3aed80895284b0f4c6b6a3c3942fb27700a104095b47aacea309f8b140ff1cc6cf165f489f21e1f05d2104eeb13810f93bfbe0a363aa15783a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ActionpackPageCachingExample::Application.config.session_store :cookie_store, key: '_actionpack-page_caching-example_session'
